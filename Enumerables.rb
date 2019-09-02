75a829cb3ad40b66eaca515d6c29072521964066
IBUNHABIBU <ibunhabib@gmail.com>
2019-09-02 05:49:40 +0300

    finished code

--- Enumerables.rb
+++ Enumerables.rb
@@ -7,7 +7,7 @@ module Enumerable
         end
     end
 
-    def my_each_with_index
+    def my_each_with_indeitem
         i = 0
         result = []
         while i < self.size
@@ -19,21 +19,52 @@ module Enumerable
 
     def my_select
         result=[]
-        my_each {|x| result<<x if yield |x|}
+        my_each {|item| result<<item if yield item}
         result
     end
 
     def my_all?
-        my_each{|x| return false unless yield x}
+        my_each{|item| return false unless yield item}
         true
     end
 
     def my_any?
-        my_each{|x| return true if yield x}
+        my_each{|item| return true if yield item}
         false
+    end
+
+    def my_none
+        my_each{|item| return false if yield item}
+        true
+    end
+
+    def my_count(arg=nil)
+        count=0
+        my_each{|item| count += 1 if yield item} if block_given? and arg.nil?
+        my_each{|item| count += 1 if arg == item} if !block_given? and !arg.nil?
+        my_each{|item| count += 1} if !block_given? and arg.nil?
+        count
+    end
+
+    def my_map(&block)
+        result = []
+        my_each{|item| result<<block.call(item)}
+        result
+    end
+
+    def my_inject(init=nil)
+        result=0
+        my_each{|item| result = yield(result,item)}
+        result
     end
 end
 weekdays=["monday","tuesday","wednesday","thursday"]
 weekdays.my_each{|day| puts day.upcase}
-weekdays.my_each_with_index{|day,x| puts "#{x}.#{day.upcase}"}
+weekdays.my_each_with_indeitem{|day,item| puts "#{item}.#{day.upcase}"}
+
+    def multiply_els(array)
+        array.my_inject(4){|item,n| item*n}
+    end
+    
+puts multiply_els([5, 7, 8,2])
 
