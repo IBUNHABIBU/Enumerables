63a5f4f6c3040efd123a5995b588a506bff7abb8
IBUNHABIBU <ibunhabib@gmail.com>
2019-09-06 13:52:07 +0300

    correcting lint errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -27,9 +27,9 @@ module Enumerable
 
   def my_all?
     if !block_given?
-     return false
+      return false
     end
-    my_each{|item| return false unless yield item}
+    my_each{ |item| return false unless yield item }
   end
 
   def my_any?
@@ -45,7 +45,7 @@ module Enumerable
 
   def my_none?
     if block_given?
-      my_each {|item| return false if yield item}
+      my_each { |item| return false if yield item }
     end
     true
   end
@@ -75,29 +75,30 @@ weekdays.my_each { |day| puts day.upcase }
 weekdays.my_each_with_index { |day, item| puts "#{item}.#{day.upcase}" }
 
 def multiply_els(array)
-  array.my_inject(1){ |prod, n| prod * n }
+  array.my_inject(1) { |prod, n| prod * n }
 end
 
-puts multiply_els([2,3,4])
+puts multiply_els([2, 3, 4])
 
-puts [2,3,4].inject(0){|sum,item| sum+item}
+puts [2, 3, 4].inject(0){|sum,item| sum+item}
 
-puts "Is  my_all? method meet the condition? : #{[2,3,4,8,34].my_all?{|item| item>4}}"
+puts "Is  my_all? method meet the condition? : #{[2, 3, 4, 8, 34].my_all?{|item| item>4}}"
 
-puts "Is  my_all when no block_given? method the meet condition? : #{[true,nil,4,8,34].my_all?}"
+puts "Is  my_all when no block_given? method the meet condition? : #{[true, nil, 4, 8, 34].my_all?}"
 
-puts "Is  my_any? method meet condition? : #{[2,4,5,8,12,34].my_any?{|item| item==12}}"
+puts "Is  my_any? method meet condition? : #{[2, 4, 5, 8, 12, 34].my_any?{|item| item==12}}"
 
-puts "Is  my_any when no block_given? method meet the condition? : #{[nil,nil,true,nil,nil].my_any?}"
+puts "Is  my_any when no block_given? method meet the condition? : #{[nil, nil, true, nil, nil].my_any?}"
 
-puts "Is  my_none? method meet the condition? :#{[2,4,5,8,34].my_none?{|item| item>91}}"
+puts "Is  my_none? method meet the condition? :#{[2, 4, 5, 8, 34].my_none?{|item| item>91}}"
 
-puts "Is  all? method meet the condition? :#{[2,4,6,5,34].all?{|item| item>1}}"
+puts "Is  all? method meet the condition? :#{[2, 4, 6, 5, 34].all?{|item| item>1}}"
 
-puts "Is  any if no block_give? method meet the condition? :#{[nil,nil,nil,nil,nil].any?}"
+puts "Is  any if no block_give? method meet the condition? :#{[nil, nil, nil, nil, nil].any?}"
 
-puts "True ,false and nil my_any? #{[false, false,true,nil].my_any?}"
+puts "True ,false and nil my_any? #{[false, false, true, nil].my_any?}"
 
-puts "when [True and nill] my_any? #{[nil, true,true].my_any?}"
+puts "when [True and nill] my_any? #{[nil, true, true].my_any?}"
 
 puts "[All nill]my_any? #{[nil, nil, nil].my_any?}"
+
